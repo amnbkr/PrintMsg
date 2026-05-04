84f5973e1da0218d9c71c567f828f66c79bb4c70
Amin <amin-abobakr@elarabygroup.com>
2026-05-04 21:47:14 +0300

    add files

---
+++ printMsg.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "printMsg.h"
+
+void printMsg(const char *msg)
+{
+    printf("Message: %s\n", msg);
+}
---
+++ printMsg.h
@@ -0,0 +1,6 @@
+#ifndef PRINTMSG_H
+#define PRINTMSG_H
+
+void printMsg(const char *msg);
+
+#endif
